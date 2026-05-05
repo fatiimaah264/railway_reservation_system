32f9568121b00c46e706386e43a7d0d85c54aabd
fatiimaah264 <ukp25cd024@gmail.com>
2026-05-05 14:02:11 +0530

    Booking feature

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 total_seats = 50
 seats = {} # Dictionary for data storage
 print("Railway System Initialized")
+
+next_booking_id = 1001
+
+def book_ticket():
+    global next_booking_id
+    name = input("Name: ")
+    age = int(input("Age: "))
+    for seat_no in range(1, 51):
+        if seat_no not in [v['seat_no'] for v in seats.values()]:
+            seats[next_booking_id] = {"name": name, "age": age, "seat_no": seat_no}
+            print(f"Booked! ID: {next_booking_id}, Seat: {seat_no}")
+            next_booking_id += 1
+            return
+    print("No seats available")
